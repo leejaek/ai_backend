099e4c9e7656e3cf35e38af946e8581b31952922
leejaek <leejh9049@gmail.com>
2025-11-30 19:46:01 +0900

    [add] Thread, Chat Entity 생성

---
+++ src/chats/entities/chat.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Thread } from '../../threads/entities/thread.entity';
+
+@Entity('chats')
+export class Chat {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('uuid')
+  threadId: string;
+
+  @ManyToOne(() => Thread, (thread) => thread.chats, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'threadId' })
+  thread: Thread;
+
+  @Column('text')
+  question: string;
+
+  @Column('text')
+  answer: string;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+}
---
+++ src/threads/entities/thread.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+  Column,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Chat } from '../../chats/entities/chat.entity';
+
+@Entity('threads')
+export class Thread {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('uuid')
+  userId: string;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @OneToMany(() => Chat, (chat) => chat.thread)
+  chats: Chat[];
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+}
